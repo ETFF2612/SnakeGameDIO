7c54409903c990e129b1d0b16798636dc35de16b
Esdras Teixeira <esdrastfalcaofilho@gmail.com>
2021-10-04 16:33:20 +0100

    definir direções

--- Snake.js
+++ Snake.js
@@ -17,8 +17,23 @@ function CreateSnake() {
         context.fillStyle = "green";
         context.fillRect (snake[i].x ,snake[i].y, box , box);
     }
+document.addEventListener('keydown', update);
+
+function update (event){
+    if(event.keyCode == 37 && direction != "Direita") direction ="Esquerda"; 
+    if(event.keyCode == 38 && direction != "Baixo") direction ="Cima";
+    if(event.keyCode == 39 && direction != "Esquerda") direction ="Direita"; 
+    if(event.keyCode == 40 && direction != "Cima") direction ="Baixo";
+}
+
+
 }
 function start(){
+    if(snake[0].x > 15 * box && direction == "Direita") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "Esquerda") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "Baixo") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "Cima") snake[0].y = 16 * box;
+
     criarBG();
     CreateSnake();
 
@@ -27,8 +42,8 @@ function start(){
 
     if (direction == "Direita") snakeX += box;
     if (direction == "Esquerda") snakeX -= box;
-    if (direction == "Cima") snakeY += box;
-    if (direction == "Baixo") snakeY -= box;
+    if (direction == "Cima") snakeY -= box;
+    if (direction == "Baixo") snakeY += box;
 
     snake.pop();
 
