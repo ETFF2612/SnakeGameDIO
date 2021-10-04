11c195ab3c1f6b6f9ef49fb553ff23ad36d462d1
Esdras Teixeira <esdrastfalcaofilho@gmail.com>
2021-10-04 18:36:01 +0100

    comida criada

--- Snake.js
+++ Snake.js
@@ -6,11 +6,13 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
-let food = {
-    x: Math.floor(Math.random() * 15 + 1),
-    y: Math.floor(Math.random() * 15 + 1)
-}
 let direction = "Direita";
+
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -21,10 +23,13 @@ function CreateSnake() {
         context.fillStyle = "green";
         context.fillRect (snake[i].x ,snake[i].y, box , box);
     }
-function comida(){
-    context.fillStyle = "red";
-    context.fillRect(food.x, food.y, box, box);
 }
+
+function createfood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x , food.y , box, box);
+}
+
 document.addEventListener('keydown', update);
 
 function update (event){
@@ -35,16 +40,19 @@ function update (event){
 }
 
 
-}
+
 function start(){
     if(snake[0].x > 15 * box && direction == "Direita") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "Esquerda") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "Baixo") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "Cima") snake[0].y = 16 * box;
 
+    
     criarBG();
     CreateSnake();
-    comida();
+    createfood();
+
+    
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y; 
